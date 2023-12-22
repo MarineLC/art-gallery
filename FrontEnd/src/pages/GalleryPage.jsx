74727709113dc0579ae5210fe84c95a6248c3147
Marine Le Cleach <lecleach.m@gmail.com>
2023-12-22 17:00:20 +0100

    display images

--- FrontEnd/src/pages/GalleryPage.jsx
+++ FrontEnd/src/pages/GalleryPage.jsx
@@ -1,45 +1,50 @@
 
 import { useEffect, useState } from "react"
+import '../App.css'
+import classeGallery from '../styles/gallery.module.css'
+
+import React from "react";
+import axios from "axios";
 
 const GalleryPage =() => {
 const [arts, setArts]= useState([])
 
 
-const fetchArts = async () => {
-    try{
-const response = await fetch('http://localhost:4000/data')
-if(response.ok){
 
-    const artsData = await response.json()
-}
-    }catch(error){
-        console.log(error)
-    }
+React.useEffect(() => {axios.get(`http://localhost:4000/arts`).then(response => {
+    console.log(response)
 
-useEffect(() => {
-    fetchArts()
+    const artsData =  response.data
+    console.log(artsData)
+    setArts(artsData);
+  })
+  
+}, []);
 
-}, [])
 
-}
+
+
 
 return(
-    <>
-    
-        <h1>All the datas</h1>
-<ul>
-    {arts.map(art => (
-        <li key ={art.id}>{art.title}</li>
-    ))}
-</ul>
+    <div className={classeGallery.ulGallery}>
 
-    </>
+    <ul>
+     {arts.map(art => (
+          <li key ={art.id}>
+            {<img src={art.img} alt={art.alt_text} width={art.width} height={art.height} />}
+         {/* <p>Title : {art.title}</p>
+          <p>Dimensions : {art.dimensions}</p>
+          <p>Artist : {art.artist_display}</p>
+          <p>Origin : {art.place_of_origin}</p>
+     <p>Medium : {art.medium_display}</p>*/}
+        </li>
+    ))}
+
+</ul>
+    </div>
 
 
 )
-
-
-
 
 
 } 
