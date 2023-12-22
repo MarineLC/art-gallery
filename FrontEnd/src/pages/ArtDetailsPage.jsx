82f0bdb861ef35e1771677106e372c7fa27b199b
Hristina Gjorgieva <hgjorgieva@gmail.com>
2023-12-22 14:09:36 +0100

    export default on all pages, body in App.css, HomePage in progress

---
+++ FrontEnd/src/pages/ArtDetailsPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ArtDetailsPage = () => {
+  return (
+    <div>
+     
+    </div>
+  );
+};
+
+export default ArtDetailsPage;
--- FrontEnd/src/pages/HomePage.jsx
+++ FrontEnd/src/pages/HomePage.jsx
@@ -1,1 +1,35 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import classes from '../styles/home.module.css';
+import '../App.css'
+
+const HomePage = () => {
+  return (
+    <div className={classes.page}>
+      <h1 className={classes.mainText}>
+        <span>
+          <Link to="/gallery" className={classes.link}>
+            ARTHUB
+          </Link>
+        </span>{' '}
+        is a digital gallery and a community for all art lovers. Every art lover is welcome to{' '}
+        <span>
+          <Link to="/contribute" className={classes.link}>
+            contribute 
+          </Link>
+        </span>{' '}
+        to the gallery and enrich our art collection. Discover the{' '}
+        <span>
+          <Link to="/team" className={classes.link}>
+            team
+          </Link>
+        </span>{' '}
+        which stands behind this project.
+      </h1>
+      <p>log in or sign up...</p>
+    </div>
+  );
+};
+
+export default HomePage;
 
