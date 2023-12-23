05a79f30c47eab2f3c499a04508b9e045923ae64
Hristina Gjorgieva <hgjorgieva@gmail.com>
2023-12-23 13:20:01 +0100

    Navbar, home, error page update

--- FrontEnd/src/components/Navbar.jsx
+++ FrontEnd/src/components/Navbar.jsx
@@ -7,7 +7,8 @@ const Navbar = () => {
     console.log('Navbar rendered')
     return (  
         <div className={classes.navbarCtn}>  
-        <Link to='/' className={classes.link}>back</Link>
+        <Link to='/' className={classes.link}>home</Link>
+        <Link to='/team' className={classes.link}>team</Link>
         <Link to='/create' className={classes.link}>add </Link>   
         <Link to='/profile' className={classes.link}>profile</Link>
         </div>
